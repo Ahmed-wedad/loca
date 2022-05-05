18e42405b006ca507a7b93b5aaf5c5f150c81ad4
ammadou <baa386006@gmail.com>
2022-05-05 23:34:47 +0200

    style

---
+++ Register.style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+import { theme } from "../myscreen.style"
+
+export const registerstyle = StyleSheet.create({
+  Button: {
+    margin: 15,
+    marginleft: 0,
+    marginRight: 0,
+  },
+  Content: {
+    padding: 15,
+    paddingTop: 0,
+  },
+  Icon: {
+    color: theme.colors.primary,
+  },
+})
